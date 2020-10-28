e22f81693bc5d1a26e4fc7def7e6ad2e22cc0438
antoine <ant.meresse@gmail.com>
2020-10-28 09:55:50 +0100

    feat :  main function

--- react.py
+++ react.py
@@ -1,5 +1,11 @@
 import os
 
 def check_if_component_direct_exist():
-    if not os.path.exists('my_folder'):
-        os.makedirs('my_folder')
+    if not os.path.exists('components'):
+        os.makedirs('components')
+
+def main():
+    check_if_component_direct_exist()
+
+if __name__ == "__main__":
+    main()
