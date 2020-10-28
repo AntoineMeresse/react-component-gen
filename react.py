bfef35b58d2de73127885fd08b771c40c514e87d
antoine <ant.meresse@gmail.com>
2020-10-28 11:07:15 +0100

    fix : UpperFirst on function name

--- react.py
+++ react.py
@@ -13,10 +13,10 @@ def createComponent(componentName : str):
     with open("components/"+cCap+".js","w+") as compJS:
         compJS.write("import React from 'react';\n")
         compJS.write("import './"+cCap+".css';\n\n")
-        compJS.write("function "+cLower+"() {\n\treturn(\n\t\t<div className=\""+cLower+"\">\n")
+        compJS.write("function "+cCap+"() {\n\treturn (\n\t\t<div className=\""+cLower+"\">\n")
         compJS.write("\t\t\t<h1>I'm a \""+cCap+"\" Component</h1>\n")
-        compJS.write("\t\t</div>\n\t)\n}\n\n")
-        compJS.write("export default "+cLower+";")
+        compJS.write("\t\t</div>\n\t);\n}\n\n")
+        compJS.write("export default "+cCap+";")
     
     # Component.css
     with open("components/"+cCap+".css","w+") as compCSS:
